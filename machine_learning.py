500c6a41d7d0074430f140ee0f62c07581bb20f1
riwu <riwu0730@gmail.com>
2017-05-14 14:58:42 +0800

    Updates

--- machine_learning.py
+++ machine_learning.py
@@ -7,7 +7,7 @@ def dump_data():
     arr = []
     x = []
     y = []
-    for i in range(10):
+    for i in range(100):
         people_in_count = random.randint(0, 10)
         people_out_count = random.randint(0, 10)
         lift_width = random.randint(30, 100)
@@ -15,7 +15,7 @@ def dump_data():
         wait_time_max = 10 + 10 + 100/30 + 5
         wait_time = people_in_count + people_out_count + 100/lift_width + lift_occupied_surface
         wait_time = wait_time / wait_time_max * 15 # normalize
-        wait_time_with_noise = random.randrange(wait_time - wait_time / 10, wait_time + wait_time / 10)
+        wait_time_with_noise = random.randint(int(wait_time - wait_time / 10), int(wait_time + wait_time / 10))
         arr.append([wait_time_with_noise, people_in_count, people_out_count, lift_width, lift_occupied_surface])
         x.append([people_in_count, people_out_count, lift_width, lift_occupied_surface])
         y.append(wait_time_with_noise)
