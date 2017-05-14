2bd44d8a92a830de242fd2bcce48ba4e590a9878
riwu <riwu0730@gmail.com>
2017-05-14 12:58:26 +0800

    Machine learning update

--- machine_learning.py
+++ machine_learning.py
@@ -11,9 +11,10 @@ def dump_data():
         people_in_count = random.randint(0, 10)
         people_out_count = random.randint(0, 10)
         lift_width = random.randint(30, 100)
-        lift_occupied_surface = random.randint(0, 50)
+        lift_occupied_surface = random.randint(0, 5)
+        wait_time_max = 10 + 10 + 100/30 + 5
         wait_time = people_in_count + people_out_count + 100/lift_width + lift_occupied_surface
-        wait_time_with_noise = random.randint(int(wait_time - wait_time / 10), int(wait_time + wait_time / 10))
+        wait_time_with_noise = random.randrange(wait_time - wait_time / 10, wait_time + wait_time / 10)
         arr.append([wait_time_with_noise, people_in_count, people_out_count, lift_width, lift_occupied_surface])
         x.append([people_in_count, people_out_count, lift_width, lift_occupied_surface])
         y.append(wait_time_with_noise)
@@ -24,10 +25,10 @@ def dump_data():
 
     svc = svm.SVC(kernel = 'linear')
     svc.fit(x, y)
-    print(svc.res)
-    plt.scatter(x)
-
-
+    print(len(x))
+    print(len(y))
+    plt.scatter([i[0] for i in x], y)
+    plt.show()
 
 dump_data()
 
