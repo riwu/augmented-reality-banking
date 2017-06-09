7b1750d541f5aa8e01856ce752ae1d8fdb172e76
riwu <riwu0730@gmail.com>
2017-06-09 23:57:58 +0800

    Added Cards table view controller

---
+++ Places/CardTableViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class CardTableViewController: UITableViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//    }
+}
